436a056c20e64f03bdc6cbcbdb205d7890cbd70b
Jarrod Kath <jarrod.kath@usq.edu.au>
2023-08-09 14:59:26 +1000

    Adding dataset and dummy test model

--- India_tea_Aug2023.R
+++ India_tea_Aug2023.R
@@ -1,2 +1,28 @@
 library(usethis)
 use_git()
+
+setwd("/Users/kathj/Documents/DELL 28th July 2022/Attachments/Projects/ASIRF/Datasets 2023")
+
+load("india_yield_climate_df.Rdata")
+
+
+##quick test gam to make sure things are working
+df<-india_yield_climate_df
+
+library(mgcv)
+
+
+summary(df$Yield..Kgs.acre.)
+
+df$district_re<-as.factor(df$District)
+
+test_gam<-gam(Yield..Kgs.acre. ~ 
+                s(dry_meanvpd,k=5) + 
+                s(dry_sumrain,k=5) + 
+                s(wet_sumsoil,k=5)  + 
+                s(Elevation) +
+                s(district_re, bs="re"),method = "REML", #+ s(year_re, bs="re"), 
+              family="gaussian", gamma=1 ,na.action = "na.fail", data=df)
+
+summary(test_gam)
+plot(test_gam, pages=1)
